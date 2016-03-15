d1625bab8a56c4e56502159d633dd641c9eeef2b
Brian Johnson <johnba4@gmail.com>
2016-03-15 13:41:38 -0400

    completed async.js challenge

--- app/async.js
+++ app/async.js
@@ -2,10 +2,37 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.asyncAnswers = {
   async : function(value) {
-
+    return new Promise(function(resolve, reject) {
+      resolve(value);
+    });
   },
 
   manipulateRemoteData : function(url) {
-
+    // Returns a promise that resolves to the sorted array of people
+    return get(url).then(JSON.parse).then(function(res) {
+      var people = res.people.map(function(item) {
+        return item.name;
+      }).sort();
+      
+      return people;
+    });
   }
 };
+
+function get(url) {
+  // Basic XHR GET, promisified
+  return new Promise(function(resolve, reject) {
+    var request = new XMLHttpRequest();
+    request.open('GET', url);
+
+    request.onload = function() {
+      if (request.status === 200) {
+        resolve(request.response);
+      } else {
+        reject(Error('XHR error'));
+      }
+    };
+
+    request.send();
+  });
+}
