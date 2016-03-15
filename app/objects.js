4f04cc3dcc3841a25e4e5316e5beb4bd93f94a5f
Brian Johnson <johnba4@gmail.com>
2016-03-15 14:24:46 -0400

    completed objects

--- app/objects.js
+++ app/objects.js
@@ -2,14 +2,27 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.objectsAnswers =  {
   alterContext : function(fn, obj) {
-
+    return fn.call(obj);
   },
 
   alterObjects : function(constructor, greeting) {
-
+    constructor.prototype.greeting = greeting;
   },
 
   iterate : function(obj) {
+    var out = [];
+    
+    // Object.keys only iterates over an objects own enumerable properties, but
+    // here I am filtering with hasOwnProperty to make it clear. For ... in loops
+    // will iterate over inherited properties, so that case the filter is necessary
+    Object.keys(obj).forEach(function(key) {
+      var pair;
+      if (obj.hasOwnProperty(key)) {
+        pair = key + ': ' + obj[key];
+        out.push(pair);
+      }
+    });
 
+    return out;
   }
 };
